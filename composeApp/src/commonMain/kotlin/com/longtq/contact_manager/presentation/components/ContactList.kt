f6e8ce52c19fa6dd4c4b5850ca04b8a41c4f8e41
longtq <tlong4856@gmail.com>
2024-10-22 17:11:34 +0700

    group contact by name

--- composeApp/src/commonMain/kotlin/com/longtq/contact_manager/presentation/components/ContactList.kt
+++ composeApp/src/commonMain/kotlin/com/longtq/contact_manager/presentation/components/ContactList.kt
@@ -1,6 +1,7 @@
 package com.longtq.contact_manager.presentation.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -20,10 +22,14 @@ import androidx.compose.material.Checkbox
 import androidx.compose.material.CheckboxDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.longtq.contact_manager.domain.model.Contact
 import contact_manager.composeapp.generated.resources.Res
 import contact_manager.composeapp.generated.resources.ic_avatar_1
@@ -45,19 +51,41 @@ fun ContactList(
     showCheckbox: Boolean,
     onCheckedChange: (Contact) -> Unit
 ) {
+    val groupedContacts = listContact.groupBy { it.name.firstOrNull()?.uppercase() ?: "#" }
+
     LazyColumn(
-        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp, vertical = 16.dp),
+        modifier = Modifier.fillMaxSize().padding( vertical = 8.dp),
         verticalArrangement = Arrangement.Top,
         contentPadding = PaddingValues(bottom = 32.dp)
     ) {
+        groupedContacts.forEach { (initial, contacts) ->
+            item {
+                Box(
+                    modifier = Modifier
+                        .padding(vertical = 16.dp)
+                        .background(Color.Gray)
+                        .fillMaxWidth()
+                        .height(50.dp),
+                    contentAlignment = Alignment.CenterStart
+                ) {
+                    Text(
+                        modifier = Modifier.padding(start = 16.dp),
+                        text = initial,
+                        color = Color.Black,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
 
-        itemsIndexed(listContact) { index, item ->
-            ContactItem(
-                contact = item,
-                showCheckbox = showCheckbox,
-                onCheckedChange = onCheckedChange
+            }
 
-            )
+            itemsIndexed(contacts) { index, item ->
+                ContactItem(
+                    contact = item,
+                    showCheckbox = showCheckbox,
+                    onCheckedChange = onCheckedChange
+                )
+            }
         }
     }
 }
@@ -68,7 +96,7 @@ fun ContactItem(
 ) {
 
     Row(
-        modifier = Modifier.fillMaxHeight().fillMaxWidth().padding(bottom = 16.dp)
+        modifier = Modifier.fillMaxHeight().fillMaxWidth().padding(bottom = 16.dp, start = 16.dp, end = 16.dp)
             .pointerInput(Unit) {
                 detectTapGestures(
                     onTap = { onCheckedChange(contact) }
